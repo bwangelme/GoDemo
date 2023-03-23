9ca87242899ea264c893cfe0048cfb8da698a5a0
xuyundong <xuyundong@douban.com>
2023-03-23 18:33:59 +0800

    rename

--- gin/main.go
+++ gin/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"bwdemo/wtypes"
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
+
+func BindDemo(c *gin.Context) {
+	var args struct {
+		IsTv wtypes.Bool `json:"is_tv"`
+	}
+	err := c.BindJSON(&args)
+	fmt.Println(args.IsTv)
+	c.JSON(200, gin.H{
+		"args": args,
+		"err":  err,
+	})
+}
+
+func Ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
 
 func main() {
 	gin.SetMode("debug")
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", Ping)
+	r.POST("/bind", BindDemo)
 	r.Run()
 }
