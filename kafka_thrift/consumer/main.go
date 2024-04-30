8b092f02e12e49d1ec2c0b909b93945d9810c343
xuyundong <xuyundong@douban.com>
2024-04-30 15:42:33 +0800

    z 20240430-154233

--- kafka_thrift/consumer/main.go
+++ kafka_thrift/consumer/main.go
@@ -1,40 +1,59 @@
 package main
 
 import (
+	"bwdemo/kafka_thrift/logger"
 	"fmt"
-	"time"
-
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost",
 		"group.id":          "thrift_consumer",
 		"auto.offset.reset": "earliest",
 	})
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
 	if err != nil {
-		panic(err)
+		logger.L.Panicf("new consumer err: %v", err)
 	}
 
-	c.SubscribeTopics([]string{"lazycat"}, nil)
+	err = c.SubscribeTopics([]string{"lazycat"}, nil)
+	if err != nil {
+		logger.L.Panicf("subscribe topic error: %v", err)
+	}
 
-	// A signal handler or similar could be used to set this to false to break the loop.
-	run := true
+	logger.L.Info("Start Consumer")
+OuterLoop:
+	for {
+		select {
+		case sig := <-sigChan:
+			logger.L.WithFields(logrus.Fields{
+				"signal": sig.String(),
+			}).Infof("get signal")
+			break OuterLoop
+		default:
+			ev := c.Poll(1000)
+			if ev == nil {
+				continue
+			}
 
-	for run {
-		msg, err := c.ReadMessage(time.Second)
-		if err == nil {
-			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-		} else if !err.(kafka.Error).IsTimeout() {
-			// The client will automatically try to recover from all errors.
-			// Timeout is not considered an error because it is raised by
-			// ReadMessage in absence of messages.
-			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+			if err == nil {
+				fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+			} else if !err.(kafka.Error).IsTimeout() {
+				// The client will automatically try to recover from all errors.
+				// Timeout is not considered an error because it is raised by
+				// ReadMessage in absence of messages.
+				fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+			}
 		}
 	}
 
+	logger.L.Info("Close Consumer")
 	c.Close()
 }
