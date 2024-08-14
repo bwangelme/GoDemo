c918e50e77ce17b97df5afc6773b9d9c7589b10e
bwangelme <bwangel.me@gmail.com>
2024-08-14 21:34:38 +0800

    add gorm demo

--- gorm/main.go
+++ gorm/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+
 	"gorm.io/gorm"
 )
 
@@ -11,28 +14,35 @@ type Product struct {
 	Price uint
 }
 
+func (*Product) TableName() string {
+	return "product"
+}
+
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	dsn := "zyb:passwd@tcp(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	// 迁移 schema
-	db.AutoMigrate(&Product{})
-
 	// Create
-	db.Create(&Product{Code: "D42", Price: 100})
+	//db.Create(&Product{Code: "D42", Price: 100})
 
 	// Read
 	var product Product
-	db.First(&product, 1)                 // 根据整型主键查找
+	db.First(&product, 3) // 根据整型主键查找
+	fmt.Println(product.ID)
+	product = Product{}
 	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+	fmt.Println(product.ID)
 
 	// Update - 将 product 的 price 更新为 200
 	db.Model(&product).Update("Price", 200)
 	// Update - 更新多个字段
 	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
 	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	fmt.Println(product.ID, product.UpdatedAt)
 
 	// Delete - 删除 product
 	db.Delete(&product, 1)
