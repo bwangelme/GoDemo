30633285b9bccb0580d7919966bf420ed2014027
bwangelme <bwangel.me@gmail.com>
2024-12-11 11:11:49 +0800

    ci z 20241211-111149

--- timestamp/main.go
+++ timestamp/main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	// 定义时间字符串
-	timeStr := "20240829113745"
+	timeStr := "2024-12-11 11:37:45.123"
 
 	// 解析时间字符串为 time.Time 类型
-	layout := "20060102150405"
+	layout := "2006-01-02 15:04:05.999"
 	t, err := time.ParseInLocation(layout, timeStr, time.Local)
 	if err != nil {
 		fmt.Println("解析时间字符串时出错:", err)
@@ -21,5 +21,5 @@ func main() {
 
 	// 输出 Unix 时间戳
 	fmt.Println("Unix 时间戳:", timestamp, t)
-	fmt.Println("time now: ", time.Now().Unix())
+	fmt.Println("time now 时间戳: ", time.Now().Unix())
 }
