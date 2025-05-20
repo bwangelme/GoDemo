d3f74545d47c2ccf7f1a0ca973f2006de8c21956
bwangelme <bwangel.me@gmail.com>
2025-05-20 14:09:05 +0800

    ci z 20250520-140905

--- slice/slice.go
+++ slice/slice.go
@@ -3,9 +3,32 @@ package main
 import (
 	"crypto/sha1"
 	"fmt"
+	"reflect"
+	"unsafe"
 )
 
 func main() {
+	EmptySlice()
+}
+
+func EmptySlice() {
+	// nil 切片的底层数组是 nil
+	// 空切片的底层数组是一个 0 值数组
+	// 所有空切片的底层都指向同一个数组
+	var s1 []int
+	var s2 = make([]int, 0)
+	var s4 = make([]int, 0)
+
+	fmt.Printf("s1 pointer:%+v, s2 pointer:%+v, s4 pointer:%+v, \n", *(*reflect.SliceHeader)(unsafe.Pointer(&s1)), *(*reflect.SliceHeader)(unsafe.Pointer(&s2)), *(*reflect.SliceHeader)(unsafe.Pointer(&s4)))
+	fmt.Printf("%v\n", (*(*reflect.SliceHeader)(unsafe.Pointer(&s1))).Data == (*(*reflect.SliceHeader)(unsafe.Pointer(&s2))).Data)
+	fmt.Printf("%v\n", (*(*reflect.SliceHeader)(unsafe.Pointer(&s2))).Data == (*(*reflect.SliceHeader)(unsafe.Pointer(&s4))).Data)
+
+	fmt.Printf("%v\n", unsafe.SliceData(s1))
+	fmt.Printf("%v\n", unsafe.SliceData(s2))
+	fmt.Printf("%v\n", unsafe.SliceData(s4))
+}
+
+func sliceOp() {
 	// 切片的end 不能超过 slice 的长度
 	i := []int{1, 2, 3, 4, 5}
 	fmt.Println(i[2:])
